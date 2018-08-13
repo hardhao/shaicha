430bc2b165575cea9844da5a6bf21cbecdc13b25
wdsuperman <425233548@qq.com>
2018-08-13 17:15:34 +0800

    finish-input

--- js/input.js
+++ js/input.js
@@ -1,3 +1,4 @@
+// 限制input输入字数
 function checkLen2(obj){
     var maxChars = 11;//最多字符数
     if (obj.value.length > maxChars){
@@ -5,3 +6,17 @@ function checkLen2(obj){
         obj.value = obj.value.substring(0,maxChars);
     }
 } 
+// input禁用滚轮
+function scrollFunc(evt) {  
+    evt = evt || window.event;  
+        if(evt.preventDefault) {  
+        // Firefox  
+        evt.preventDefault();  
+        evt.stopPropagation();  
+        } else {  
+        // IE  
+        evt.cancelBubble=true;  
+        evt.returnValue = false;  
+    }  
+    return false;  
+    } 
