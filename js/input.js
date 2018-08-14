4734d0838b0417f1c2a8939eb6db6c96da8d4e0e
wdsuperman <425233548@qq.com>
2018-08-14 14:36:20 +0800

    zishiying

--- js/input.js
+++ js/input.js
@@ -2,7 +2,7 @@
 function checkLen2(obj){
     var maxChars = 11;//最多字符数
     if (obj.value.length > maxChars){
-        alert("最多输入11个字符，现已超出限制，自动截取前30个字符！");
+        alert("最多输入11个字符，现已超出限制，自动截取前11个字符！");
         obj.value = obj.value.substring(0,maxChars);
     }
 } 
